b7988877759f715fb88c158744a4e014fb12471a
pablo.terradillos <pablo.terradillos@etermax.com>
2014-04-15 11:38:33 -0300

    Forced high accuracy

--- main.js
+++ main.js
@@ -13,18 +13,29 @@ var watcher;
 
 var follow_button = document.getElementsByClassName('follow')[0];
 var unfollow_button = document.getElementsByClassName('unfollow')[0];
+var alert_modal = document.getElementsByClassName('waiting')[0];
+var error_modal = document.getElementsByClassName('error')[0];
 
 
 follow_button.addEventListener('click', function() {
  var polyline = L.polyline([], {color: 'red'}).addTo(map);
 
+ map.locate({setView: true, maxZoom: 100});
+
  follow_button.style.display = 'none';
  unfollow_button.style.display = 'block';
+ alert_modal.style.display = 'block';
 
  watcher = navigator.geolocation.watchPosition(function(position) {
-    map.setView([position.coords.latitude, position.coords.longitude], 100);
+    alert_modal.style.display = 'none';
+
     marker.setLatLng([position.coords.latitude, position.coords.longitude]);
     polyline.addLatLng([position.coords.latitude, position.coords.longitude]); 
+  }, function() {
+      alert_modal.style.display = 'none';
+      error_modal.style.display = 'block';
+  }, {
+    enableHighAccuracy: true
   });
 });
 
